ced4cdc1729a9b1d46f1d9b41e3322921f868b0f
Dhanada Panda <130206432+Dhanada-Panda@users.noreply.github.com>
2025-01-03 20:26:07 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 ###import google.generativeai as genai
-#api_key = "AIzaSyCBZcIFlUoHfcOObHHYVzcizN2X7GPyYgE" 
+#api_key = "api_key" 
 #genai.configure(api_key=api_key)
 #model = genai.GenerativeModel("gemini-1.5-flash")
 #response = model.generate_content("Explain how AI works")
